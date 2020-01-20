3596b599ac8c9fe4f3de0ba9cbf868465c1609e6
Marcos Del Valle <51240229+mansodelvalle@users.noreply.github.com>
2020-01-20 02:40:19 -0300

    Update Database.class.php

--- app/class/Database.class.php
+++ app/class/Database.class.php
@@ -11,10 +11,10 @@ class Database
     public function conectar() {
         try {
             // Conexao com banco MySQL
-            $host = "mysql669.umbler.com:41890";
-            $name = "teatrofacil";
-            $user = "teatrofacil";
-            $pass = "marcosdelvalle";
+            $host = "localhost";
+            $name = "bancodedados";
+            $user = "root";
+            $pass = "";
             $this->conn = new PDO("mysql:host={$host};dbname={$name}", $user, $pass);
     
             // Define para que o PDO lance exceções na ocorrência de erros
